476002289632fa1c118bebcdca6e97f7dde3ec94
Sofiakhis <sofiakhismatova@mail.ru>
2024-05-20 17:08:55 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,13 @@
+immutable_var = 1, 2, 3, 'манго', True
+mutable_list = [1, 2, 2, 'персик']
+mutable_list[0] = 4
+print(type(immutable_var))
+print(mutable_list)
+print(type(mutable_list))
+
+
+
+
+
+
+
